2ac14cc6665949cdba9d5b3f8727bf24dd52e198
Leo Guedes <leoguedesdev@gmail.com>
2025-01-13 21:28:25 -0300

    first commit

---
+++ src/main/java/leoguedex/com/github/gestao_erp_curso/GestaoErpCursoApplication.java
@@ -0,0 +1,12 @@
+package leoguedex.com.github.gestao_erp_curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoErpCursoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoErpCursoApplication.class, args);
+	}
+}
---
+++ src/test/java/leoguedex/com/github/gestao_erp_curso/GestaoErpCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package leoguedex.com.github.gestao_erp_curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoErpCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
