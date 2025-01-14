9317dabca1a7809e0ab2eec4e8531c01d9bdacb2
Leo Guedes <leoguedesdev@gmail.com>
2025-01-13 22:18:08 -0300

    criacao da classe person, aula do dia 13/01, com Evandro, Kaio, Daina, Wellington

---
+++ src/main/java/leoguedex/com/github/gestao_erp_curso/domain/Person.java
@@ -0,0 +1,19 @@
+package leoguedex.com.github.gestao_erp_curso.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class Person {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+}
