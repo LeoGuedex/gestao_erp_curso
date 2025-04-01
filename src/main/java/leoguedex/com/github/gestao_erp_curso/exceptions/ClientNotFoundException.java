6691e3eb12d6d5bbee7165b095927d50bc2a84f8
Leo Guedes <leoguedesdev@gmail.com>
2025-04-01 20:16:11 -0300

    Mudança de teste visibilidade

--- src/main/java/leoguedex/com/github/gestao_erp_curso/exceptions/ClientNotFoundException.java
+++ src/main/java/leoguedex/com/github/gestao_erp_curso/exceptions/ClientNotFoundException.java
@@ -2,6 +2,7 @@ package leoguedex.com.github.gestao_erp_curso.exceptions;
 
 public class ClientNotFoundException extends RuntimeException {
 
+  //Leo was here
   public ClientNotFoundException(String message) {
     super(message);
   }
